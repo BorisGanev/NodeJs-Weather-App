83dca9ade5ba718ab82ab14aa30cfa14f388490f
DingoTheMagic <boris.ganev@hotmail.com>
2017-08-07 22:47:09 +0100

    Dynamic weather retrieval through DarkSky API
    Application can now dynamically retrieve weather based on the user
    inputed geocode address.
    Weather logic is stored and handled in the newly created
    'weather\weather.js'.
    
    -app.js:
    *Integration of the geocode and weather logic.
    
    +weather\weather.js:
    *Integration with DarkSky API.
    *Creation of weather logic, which fetches weather data about a inputted
    lat and lng through the Dark Sky API.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const geocode = require('./geocode/geocode.js');
+const weather = require('./weather/weather.js');
 const yargs = require('yargs');
 
 const argv = yargs
@@ -18,6 +19,13 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     if (errorMessage) {
         console.log(errorMessage);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+            }
+        });
     }
 });
